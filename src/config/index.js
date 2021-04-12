1d2ff650a956e9e5f51da745450a6c8a322c76b4
Bahtiar <zam@kitalulus.com>
2021-04-13 04:25:47 +0700

    update: env and scripst

--- src/config/index.js
+++ src/config/index.js
@@ -3,9 +3,14 @@ import { config } from "dotenv";
 config();
 
 export default {
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASS: process.env.DB_PASS,
   DB_URI: process.env.DB_URI,
   expressURI: process.env.EXPRESS_PORT ?? 80,
   db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_CONN,
     protocol: process.env.DB_CONN,
     pool: {
